f9b527eb347c0a95eee50534d1998481ea99e9cc
Vaibhav Suryavanshi <128031884+VaibhavSuryavanshi93@users.noreply.github.com>
2026-01-20 18:52:23 +0530

    case-sensitive-error

--- src/pages/GptSearch.jsx
+++ src/pages/GptSearch.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import GptSearchBar from "./gptSearchBar";
+import GptSearchBar from "./GptSearchBar";
 import GptMovieSuggestion from "./GptMovieSuggestion";
 import { BG_URL } from "../utils/constants";
 import Footer from "./Footer";
